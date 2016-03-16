6fde050e452932d5ef2d85cfe3690518b8f7c2d6
俞凯 <ykymy@192.168.3.148>
2016-03-16 16:02:03 +0800

    Initial Commit

---
+++ TextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextView
+//
+//  Created by 俞凯 on 16/3/16.
+//  Copyright © 2016年 俞凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextView
+//
+//  Created by 俞凯 on 16/3/16.
+//  Copyright © 2016年 俞凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
